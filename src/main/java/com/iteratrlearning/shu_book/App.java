928ad9f98339159d326e76ab110bd7e8cad802c2
unknown <jongyeonsim2@gmail.com>
2024-06-07 10:32:06 +0900

    1st commit

---
+++ src/main/java/com/iteratrlearning/shu_book/App.java
@@ -0,0 +1,13 @@
+package com.iteratrlearning.shu_book;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
